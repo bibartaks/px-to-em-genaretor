4f356baf9263d44e4a9a9d2df5434d6a6faa047e
Bibarta Kumar Sarkar <bibartaks@gmail.com>
2023-07-23 21:30:20 +0600

    Added pwa

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-let defaultInput = document.getElementById('defaultSize')
-let convertInput = document.getElementById('convertEm')  
+let defaultInput = document.getElementById("defaultSize")
+let convertInput = document.getElementById("convertEm")
 
 function result() {
-    return convertInput.value / defaultInput.value
+  return convertInput.value / defaultInput.value
 }
 
-document.getElementById('convert').addEventListener('click', function(){
-    // let result = convertInput.value / defaultInput.value
-    if(defaultInput.value && convertInput.value) {
-        document.getElementById('result').innerHTML = "Result: " + result() + "em"
-    } 
+document.getElementById("convert").addEventListener("click", function () {
+  // let result = convertInput.value / defaultInput.value
+  if (defaultInput.value && convertInput.value) {
+    document.getElementById("result").innerHTML = "Result: " + result() + "em"
+  }
 })
